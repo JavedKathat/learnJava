a4b71d3678f7b6fd00ab07c0c5661458c07d2132
Javed Kathat <javedkhankathat3040@gmail.com>
2024-12-30 22:14:55 +0530

    Java program to insert element to the index

---
+++ Java_Dec_24/30_Dec_24/InsertAtLoc.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+
+public class InsertAtLoc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array a : ");
+        int n = sc.nextInt();
+
+        System.out.println("Enter array a : ");
+        int a[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        System.out.println("Array a is : ");
+        for(int x: a) {
+            System.out.print("| "+x+" ");
+        }
+        System.out.println("|");
+
+        System.out.println("Enter an element to insert");
+        int val = sc.nextInt();
+
+        System.out.println("Enter index location to insert : ");
+        int loc = sc.nextInt();
+
+        if(loc < 0 || loc >= n) {
+            System.out.println("Please enter a valid index");
+        }else {
+            int b[] = new int[n+1];
+
+            for (int i = 0, j = 0; i < n; i++, j++) {
+                if(i == loc) {
+                    b[j]= val;
+                    b[j+1] = a[i];
+                    ++j;
+                }else{
+                    b[j] = a[i];
+                }
+            }
+
+            System.out.println("Array b is : ");
+            for(int i = 0; i < n+1; i++) {
+                System.out.print("| "+b[i]+" ");
+            }
+        }
+    }
+}
